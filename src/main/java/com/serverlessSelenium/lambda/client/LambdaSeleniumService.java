2c63a678767ab8c8ec54a7a50362601af543e24f
Ehab <ehabanshad@yahoo.com>
2020-07-01 22:18:16 +0100

    separate the client

--- src/main/java/com/serverlessSelenium/lambda/client/LambdaSeleniumService.java
+++ src/main/java/com/serverlessSelenium/lambda/client/LambdaSeleniumService.java
@@ -1,6 +1,7 @@
-package com.serverlessSelenium.lambda;
+package com.serverlessSelenium.lambda.client;
 
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
+import com.serverlessSelenium.lambda.TestRequest;
 import com.serverlessSelenium.lambda.TestResult;
 
 public interface LambdaSeleniumService {
--- src/main/java/com/serverlessSelenium/lambda/client/TestInvoker.java
+++ src/main/java/com/serverlessSelenium/lambda/client/TestInvoker.java
@@ -1,7 +1,9 @@
-package com.serverlessSelenium.lambda;
+package com.serverlessSelenium.lambda.client;
 
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
+import com.serverlessSelenium.lambda.TestRequest;
+import com.serverlessSelenium.lambda.TestResult;
 
 public class TestInvoker {
 
